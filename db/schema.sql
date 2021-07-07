9c2524b18320aed2e46552b3a339acd04ab1d2b1
Ody <melodymonyok@gmail.com>
2021-07-07 12:21:04 -0500

    Updated folder structure.

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary INT,
+    department_id INT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+	PRIMARY KEY(id)
+);
